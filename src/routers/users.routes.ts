ba4d291226ad97fb2f721571ce1491666d79b591
Andrés Felipe Rodríguez Gallego <79536152+andres-rod-g@users.noreply.github.com>
2025-02-23 15:05:12 -0500

    Update users.routes.ts

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -17,4 +17,15 @@ userRouter.post("/", (req, res) => {
     res.status(200).json('Hola Mundo JEJEJEJE ' + req.body.nombre)
 })
 
+// Endpoint GET a la ruta "/views/", donde cuando se consulte, retorne la cantidad de 
+// vistas que hayy al mismo tiempo, aumente la variable de las vistas en 1. 
+// Y también decir la hora actual.
+
+// EndPoint POST, en la ruta "/math/sum", donde tome de un diccionario los valores
+// con llave "val1" y "val2" y los sume. Verificar si son son números, y verificar
+// que no sean números pares
+
+// Los EndPoints deben tener buenas prácticas que se han enseñado, como el uso de los
+// enrutadores
+
 export default userRouter
